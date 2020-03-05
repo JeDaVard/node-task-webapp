3e303f9275920d0464ad2f164b91c7f857f279e7
Davit Vardanyan <jedavard@gmail.com>
2020-03-06 00:41:12 +0400

    fully featured user authentication and authorization integrated

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') return res.status(503).send('<h1>The website is under construction. Please try later</h1>');
+//     next()
+// })
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -17,20 +18,37 @@ router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const token = await user.authToken();
-
+        
         res.send({ user, token });
     } catch(e) {
         res.status(500).send('Undefined yet' + e)
     }
 });
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
     try {
-        const users = await User.find({});
-        res.send(users)
-    } catch(e) {
-        res.status(500).send()
+        req.user.tokens = req.user.tokens.filter( token => token.token !== req.token );
+        await req.user.save();
+
+        res.status(200).send('Logged out');
+    } catch (e) {
+        res.status(500).send();
     }
+});
+
+router.post('/users/logoutall', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+
+        res.status(200).send('Logged out from all devices');
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
 })
 
 router.get('/users/:id', async (req, res) => {
