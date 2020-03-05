50c8aa599bcb48aa4831283323dac5010fb5e8ed
Davit Vardanyan <jedavard@gmail.com>
2020-03-05 16:52:08 +0400

    email model and login integration

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
@@ -7,15 +8,37 @@ const userSchema = new mongoose.Schema({
         trim: true,
         required: true
     },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
     password: {
         type: String,
         trim: true,
         required: true,
-        minlength: 6,
-        save() {}
+        minlength: 6
     }
 })
 
+ userSchema.statics.findByCredentials = async function(email, password){
+     const user = await User.findOne({email});
+     if (!user) throw new Error('We haven\'t account with this email');
+    
+     if (password === undefined) throw new Error('Fill your password');
+     const isMatchedPass = await bcrypt.compare(password, user.password);
+     if (!isMatchedPass) throw new Error('Incorrect password');
+
+     return user
+ }
+ 
 userSchema.pre('save', async function(next) {
     const user = this;
     
