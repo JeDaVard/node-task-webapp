f573f0160ec8372e50ecf2f21ea3f5a23e3b29f2
Davit Vardanyan <jedavard@gmail.com>
2020-03-08 02:47:46 +0400

    ready for heroku

---
+++ public/js/clientside.js
@@ -0,0 +1,5 @@
+fetch('/users').then((response) => {
+    response.json().then((users) => {
+        console.log(users)
+    })
+})
--- src/index.js
+++ src/index.js
@@ -8,18 +8,25 @@ const hbs = require('hbs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const publicDirPath = path.join(__dirname, '../public')
-const partialPath = path.join(__dirname, '../templates/partials')
-const viewsDirPath = path.join(__dirname, '../templates/views')
+const publicDirPath = path.join(__dirname, '../public');
+const partialPath = path.join(__dirname, '../templates/partials');
+const viewsDirPath = path.join(__dirname, '../templates/views');
 
-app.set('view engine', 'hbs')
-app.set('views', viewsDirPath)
-hbs.registerPartials(partialPath)
+app.set('view engine', 'hbs');
+app.set('views', viewsDirPath);
+hbs.registerPartials(partialPath);
 
-app.use(express.static(publicDirPath))
+app.use(express.static(publicDirPath));
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Davit Vardanyan'
+    })
+})
 
 app.listen(port, () => {
     console.log('server is up on port ' + port)
