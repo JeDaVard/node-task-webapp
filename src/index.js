dfe605a548cecb7e3ba18c7ca42d6b49b4633d96
Davit Vardanyan <jedavard@gmail.com>
2020-03-08 14:21:12 +0400

    ready for tests

--- src/index.js
+++ src/index.js
@@ -2,21 +2,9 @@ const express = require('express');
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
-const path = require('path');
-const hbs = require('hbs');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const publicDirPath = path.join(__dirname, '../public');
-const partialPath = path.join(__dirname, '../templates/partials');
-const viewsDirPath = path.join(__dirname, '../templates/views');
-
-app.set('view engine', 'hbs');
-app.set('views', viewsDirPath);
-hbs.registerPartials(partialPath);
-
-app.use(express.static(publicDirPath));
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
